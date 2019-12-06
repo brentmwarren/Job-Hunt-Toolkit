8c4f637c3ef9ce487ebc7083eeaf7713e1143455
Brent Warren <macuser@Brentsmac.local>
2019-12-05 18:06:37 -0800

    full crud on applications complete

--- job_hunt_toolkit/urls.py
+++ job_hunt_toolkit/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.application_list, name='application_list'),
     path('application/new/', views.application_create, name='application_create'),
-    # path('applications/<int:pk>/edit', views.application_edit, name='application_edit'),
+    path('applications/<int:pk>', views.application_detail, name='application_detail'),
+    path('applications/<int:pk>/edit', views.application_edit, name='application_edit'),
+    path('applications/<int:pk>/delete', views.application_delete, name='application_delete'),
+
     # path('checklist/', views.checklist_list, name='checklist_list')
 ]
--- job_hunt_toolkit/views.py
+++ job_hunt_toolkit/views.py
@@ -31,16 +31,21 @@ def application_list(request):
     applications = Application.objects.all()
     return render(request, 'application_list.html', {'applications': applications})
 
+
 def application_create(request):
     if request.method == 'POST':
         form = ApplicationForm(request.POST)
         if form.is_valid():
             # application["user"] = "put in user id from django admin portal"
             application = form.save()
-            return redirect('application_list')
+            return redirect('application_list', pk=application.pk)
     else:
         form = ApplicationForm()
     return render(request, 'application_form.html', {'form': form}) 
+
+def application_detail(request, pk):
+    application = Application.objects.get(id=pk)
+    return render(request, 'application_detail.html', {'application': application})       
 
 def application_edit(request, pk):
     application = Application.objects.get(pk=pk)
@@ -48,10 +53,14 @@ def application_edit(request, pk):
         form = ApplicationForm(request.POST, instance=application)
         if form.is_valid():
             artist = form.save()
-            return redirect('application_list')
+            return redirect('application_list', pk=application.pk)
     else:
         form = ApplicationForm(instance=application)
-    return render(request, 'application_form.html', {'form': form})    
+    return render(request, 'application_form.html', {'form': form})   
+
+def application_delete(request, pk):
+    Application.objects.get(id=pk).delete()
+    return redirect('application_list')     
 
 # def checklist_list(request):
 #     checklist = Checklist.objects.all()
